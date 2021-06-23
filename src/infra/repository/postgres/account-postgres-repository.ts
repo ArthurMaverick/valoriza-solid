5a3fdc909bed9e62e77994ec9424266a9f7fbe44
ArthurMaverick <arthuracs18@gmail.com>
2021-06-23 14:43:11 -0300

    valoriza aula 3

--- src/infra/repository/postgres/account-postgres-repository.ts
+++ src/infra/repository/postgres/account-postgres-repository.ts
@@ -1,10 +1,20 @@
 import { PostgresUtils } from './helpers/postgres-utils'
-import { AddAccountRepository, CheckAccountByEmailRepository, FindUserAccountRepository } from '../../../data/rules/db'
+import {
+  AddAccountRepository,
+  CheckAccountByEmailRepository,
+  FindUserAccountRepository,
+  CheckTagByNameRepository,
+  AddTagAccountRepository,
+  FindTagAccountRepository
+} from '../../../data/rules/db'
 
 export class AccountPostgresRepository implements
   CheckAccountByEmailRepository,
+  CheckTagByNameRepository,
   AddAccountRepository,
-  FindUserAccountRepository {
+  AddTagAccountRepository,
+  FindUserAccountRepository,
+  FindTagAccountRepository {
     private readonly PostgresHelper: PostgresUtils
     constructor (config: object) {
       this.PostgresHelper = new PostgresUtils(config)
@@ -17,6 +27,13 @@ export class AccountPostgresRepository implements
       return isvalid.rows[0]
     }
 
+    async checkByTagName (tagName: string): Promise<CheckTagByNameRepository.Result> {
+      await this.PostgresHelper.createConnection()
+      const isvalid = await this.PostgresHelper.client
+        .query(`SELECT email FROM nlw WHERE name='${tagName}'`)
+      return isvalid.rows[0]
+    }
+
     async addUser (data: AddAccountRepository.Params): Promise<AddAccountRepository.Result> {
       await this.PostgresHelper.createConnection()
       await this.PostgresHelper.client
@@ -26,7 +43,22 @@ export class AccountPostgresRepository implements
       return response.rows[0]
     }
 
+    async addTags (data: AddTagAccountRepository.Params): Promise<AddTagAccountRepository.Result> {
+      await this.PostgresHelper.createConnection()
+      await this.PostgresHelper.client
+        .query(`INSERT INTO nlw (id,name) values ('${data.id}','${data.name}')`)
+      const response = await this.PostgresHelper.client
+        .query(`SELECT * FROM nlw WHERE id='${data.id}'`)
+      return response.rows[0]
+    }
+
     async findUserById (id: FindUserAccountRepository.Params):Promise<FindUserAccountRepository.Result> {
+      const response = await this.PostgresHelper.client
+        .query(`SELECT * FROM nlw WHERE id='${id}'`)
+      return response.rows[0]
+    }
+
+    async findTagById (id: FindTagAccountRepository.Params): Promise<FindTagAccountRepository.Result> {
       const response = await this.PostgresHelper.client
         .query(`SELECT * FROM nlw WHERE id='${id}'`)
       return response.rows[0]
--- src/main/factories/usecases/add-account-factory.ts
+++ src/main/factories/usecases/add-account-factory.ts
@@ -5,11 +5,17 @@ import { AddIdOnRepository } from '../../../infra/criptography/add-id'
 
 export const makeDbAddUser = (): CreateUser => {
   const uuid = new AddIdOnRepository()
-  const PostgresRepository = new AccountPostgresRepository()
+  const PostgresRepository = new AccountPostgresRepository({
+    user: process.env.NAME,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    host: process.env.HOSTNAME,
+    port: process.env.PORT
+  })
   return new DbAddUser(
     uuid,
-    PostgresRepository,
-    PostgresRepository,
-    PostgresRepository
+    PostgresRepository, // validade
+    PostgresRepository, // find
+    PostgresRepository // create
   )
 }
